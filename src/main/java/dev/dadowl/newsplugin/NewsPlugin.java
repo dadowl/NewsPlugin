d03d1ba94e4358732cae4406a1cf44c0e959d005
DadOwl <me@dadowl.dev>
2022-05-13 21:55:48 +0700

    init

---
+++ src/main/java/dev/dadowl/newsplugin/NewsPlugin.java
@@ -0,0 +1,17 @@
+package dev.dadowl.newsplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NewsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
